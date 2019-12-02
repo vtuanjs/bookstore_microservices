dea83f54a37999839fd4ba67f6d729ad4abf0b14
Nguyễn Văn Tuấn <vantuan130393@gmail.com>
2019-12-02 15:53:37 +0700

    update docker

--- books/index.js
+++ books/index.js
@@ -2,7 +2,7 @@ const app = require("./app")
 const database = require("./database");
 
 database.connect().then(() => {
-  app.listen(4001, () => {
+  app.listen(process.env.PORT, () => {
     console.log("Book services running at port 4001...");
   });
 });
