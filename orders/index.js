ee9b3479941959ebb710645f56403c1dc0c63b7b
Nguyễn Văn Tuấn <vantuan130393@gmail.com>
2019-12-02 15:54:59 +0700

    update docker

--- orders/index.js
+++ orders/index.js
@@ -2,7 +2,7 @@ const app = require("./app")
 const database = require("./database");
 
 database.connect().then(() => {
-  app.listen(4002, () => {
+  app.listen(process.env.PORT, () => {
     console.log("Order services running...");
   });
 });
