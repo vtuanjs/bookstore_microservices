cd68e0caa9913c496daf38272095bacc28607dfe
Nguyễn Văn Tuấn <vantuan130393@gmail.com>
2019-12-02 15:54:12 +0700

    update docker

--- customers/index.js
+++ customers/index.js
@@ -2,7 +2,7 @@ const app = require("./app")
 const database = require("./database");
 
 database.connect().then(() => {
-  app.listen(4002, () => {
+  app.listen(process.env.PORT, () => {
     console.log("Customer services running at port 4002...");
   });
 });
